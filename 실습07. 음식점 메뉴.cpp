30d0341bc23df2b50866bd5d4a1f3676b3243771
marumaru99 <102706594+marumaru99@users.noreply.github.com>
2023-02-16 21:43:45 +0900

    Add files via upload

---
+++ 실습07. 음식점 메뉴.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Food
+{
+public:
+	int make_time;
+	string description;
+	void food() { cout << "this is food" << endl; }
+	void foodInfo() { cout << make_time << description << endl; }
+};
+
+//foodInfo를 통해 음식이 만들어지는 시간과 음식에 대한 설명 서술
+
+class Ramen :public Food
+{
+public:
+	
+
+class Spagetti :public Food
+{
+public:
+
+};
+
+class Udon :public Food
+{
+public:
+	
+};
+
+int main()
+{
+	Food menu[3];
+	//메뉴판 출력
+	for(i=0;i<)
+
+	return 0;
+}
